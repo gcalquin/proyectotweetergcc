241c6e8ba30227d3b84e349301830f17ac2ccbb6
gcalquin <gcalquin@gmail.com>
2021-06-20 18:31:01 -0400

    Trabajo para hastags

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,7 @@
 class Tweet < ApplicationRecord
+ include ActionView::Helpers::UrlHelper
+
+  before_save :hashtag
   belongs_to :user
   has_many :likes
   has_many :liking_users, :through => :likes, :source => :user
@@ -17,6 +20,8 @@ class Tweet < ApplicationRecord
     end
   end
 
+  
+
 
   def remove_like(user)
     Like.where(user: user, tweet: self).first.destroy
@@ -25,6 +30,21 @@ class Tweet < ApplicationRecord
   def add_like(user)
     Like.create(user: user, tweet: self)    
   end
+
+
+
+  def hashtag
+    arreglo = []
+    self.contenido.split(" ").each do |p|
+      if p.start_with?("#")
+        p_parsed = p.sub "#", "%23"
+        p = link_to( p, Rails.application.routes.url_helpers.root_path+"?search="+p_parsed )
+      end
+      arreglo.push(p)
+    end
+    self.contenido = arreglo.join(" ")
+  end
+
   
   def count_rt
     Tweet.where(retweeteado: self.id).count
